aa98aff882ef63b5e98f25fa3c094a850c913444
Filipe Angelo <75441666+SkylipeBR2@users.noreply.github.com>
2023-11-05 23:22:40 -0300

    primeiraFase
    primeira fase - completa

--- Game.java
+++ Game.java
@@ -33,6 +33,19 @@ public class Game {
         int U2 = random.nextInt(10);
         int V2 = random.nextInt(10);
 
+        int K3 = random.nextInt(10);
+        int L3 = random.nextInt(10);
+        int M3 = random.nextInt(10);
+        int O3 = random.nextInt(10);
+        int N3 = random.nextInt(10);
+        int P3 = random.nextInt(10);
+        int Q3 = random.nextInt(10);
+        int R3 = random.nextInt(10);
+        int S3 = random.nextInt(10);
+        int T3 = random.nextInt(10);
+        int U3 = random.nextInt(10);
+        int V3 = random.nextInt(10);
+
 
         System.out.printf("{%d*%d+[%d(%d*%d)+%d+%d]-%d*%d}\n", K, L, M, N, O, P, Q,R,S);
         int resultadoJogador = input.nextInt();
@@ -42,7 +55,11 @@ public class Game {
         int segundoResultadoJogador = input.nextInt();
         int resultado2 = (K2*L2+(M2*(N2*O2)-P2+Q2)-R2*S2);
 
-        int resultado = resultado1 + resultado2;
+        System.out.printf("{%d*%d+[%d(%d*%d)-%d+%d]-%d*%d}\n", K3, L3, M3, N3, O3, P3, Q3, R3, S3);
+        int terceiroResultadoJogador = input.nextInt();
+        int resultado3 = (K3*L3+(M3*(N3*O3)-P3+Q3)-R3*S3);
+
+        int resultado = resultado1 + resultado2 + resultado3;
         System.out.printf("Resultado: %d \n", resultado);
         if (resultado == resultadoJogador){
             System.out.println("Acertou!");
---
+++ out/production/Grupo-1---Turma-A/README.md
@@ -0,0 +1,12 @@
+# Grupo-1---Turma-A
+Projeto integrador: RPG "Echos of Dungeons".
+
+Feito por:
+- Filipe Angelo de Oliveira
+- Heloisa Mendes do Nascimento Barbosa
+- Kevin Stenio Fernandes Lacerda Duarte
+- Renan França de Jorge
+
+
+### Echos of Dungeons - Prólogo
+“Dois irmãos que gostam de aventuras e mistérios se deparam presos em uma masmorra abandonada. Cabe a eles decifrar os enigmas juntos para saírem da masmorra com novos ensinamentos.” 
